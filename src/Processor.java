1e04b69d2f9b879f08675f01ba4870cdcce92b2e
Matteo Alesiani <teo97.alesiani@gmail.com>
2016-08-24 17:48:03 +0200

    Update the work did for the exam
    Il codice effettua il riconoscimento facciale prendendo uno Stream video
    dalla telecamera del computer.

---
+++ src/Processor.java
@@ -0,0 +1,53 @@
+ import org.opencv.core.Mat;  
+ import org.opencv.core.MatOfRect;  
+ import org.opencv.core.Point;  
+ import org.opencv.core.Rect;  
+ import org.opencv.core.Scalar;  
+ import org.opencv.core.Size;  
+ 
+ import org.opencv.imgproc.Imgproc;  
+ import org.opencv.objdetect.CascadeClassifier;  
+ 
+   
+ public class Processor{
+	 
+	 private CascadeClassifier face_cascade;  
+	 // Create a constructor method  
+      
+	public Processor(){  
+    	face_cascade = new CascadeClassifier("haarcascade_frontalface_alt.xml");  
+           
+    	if(face_cascade.empty())  
+        {  
+    		System.out.println("--(!)Error loading A\n");  
+    		return;  
+        }  
+        else  
+        	System.out.println("Face classifier loooaaaaaded up");  
+      }
+      
+	public Mat detect(Mat inputframe){  
+		
+		
+		Mat mRgba=new Mat();  
+		Mat mGrey=new Mat(); 
+		
+		MatOfRect faces = new MatOfRect();  
+		inputframe.copyTo(mRgba);  
+		inputframe.copyTo(mGrey);  
+		Imgproc.cvtColor( mRgba, mGrey, Imgproc.COLOR_BGR2GRAY);  
+		Imgproc.equalizeHist( mGrey, mGrey );  
+		face_cascade.detectMultiScale(mGrey, faces);  
+		
+		System.out.println(String.format("Detected" + faces.toArray().length + " faces"));  
+		for(Rect rect:faces.toArray())  
+		{  
+			Point center= new Point(rect.x + rect.width*0.5, rect.y + rect.height*0.5 );  
+			//trasformare in rettangolo
+			Imgproc.ellipse( mRgba, center, new Size( rect.width*0.5, rect.height*0.5), 0, 0, 360, new Scalar( 255, 0, 255 ), 4, 8, 0 );  
+		}  
+		
+		return mRgba;  
+	}  
+}  
+ 
